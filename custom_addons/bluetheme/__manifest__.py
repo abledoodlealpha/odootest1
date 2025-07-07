408cfb836c7f18c8225d4821d15f6f0a7c4eb55c
abledoodlealpha <amandubey2269@gmail.com>
2025-07-07 06:13:40 +0530

    added custom POS changes

---
+++ custom_addons/bluetheme/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Blue Theme',
+    'version': '1.0',
+    'category': 'Theme/Creative',
+    'summary': 'Custom Blue Color Theme for Odoo frontend',
+    'description': """
+    A clean blue color theme with multiple blue shades for Odoo frontend UI customization.
+    """,
+    'author': 'Aman',
+    'website': 'https://yourwebsite.com',
+    'depends': ['web', 'website'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
